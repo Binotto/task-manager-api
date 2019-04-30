123beae6bf452a70574c530b4dedb447ba92a268
Matheus <Binotto>
2019-04-30 18:52:36 -0300

    Resource Creation Endpoints: Part I

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,62 +1,10 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
-//Mongoose Model User
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value){
-            if (!validator.isEmail(value)){
-                throw new Error('Email is invalid!')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 6,
-        validate(value){
-        if(value.toLowerCase().includes('password')){
-                throw new Error('You can not create a password with the word "password".')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate (value){
-            if(value < 0){
-                throw new Error('Age must be a positive number.')
-            }
-        }
-    }
-})
-
-// const me = new User({
-//     name: '    Matheus    ',
-//     password: ' senha         ',
-//     email: 'MATHEUS.BINOTTO@HOTMAIL.COM     '
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
 
 //Mongoose Model Task
 const Task = mongoose.model('Task', {
@@ -70,14 +18,4 @@ const Task = mongoose.model('Task', {
         required: false,
         default: false
     }
-})
-
-const task = new Task({
-    description: 'Go to work.',
-})
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log('Error!', error)
 })
---
+++ src/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+//Mongoose Model User
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if (!validator.isEmail(value)){
+                throw new Error('Email is invalid!')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 6,
+        validate(value){
+        if(value.toLowerCase().includes('password')){
+                throw new Error('You can not create a password with the word "password".')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate (value){
+            if(value < 0){
+                throw new Error('Age must be a positive number.')
+            }
+        }
+    }
+})
+
+module.exports = User
