23fd023a9d0429088ba7cbe1f518c42bc6f19c8d
Matheus <Binotto>
2019-05-01 12:46:09 -0300

    Resource Reading Endpoints: Part I

--- src/index.js
+++ src/index.js
@@ -8,6 +8,28 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((error) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
 app.post('/users', (req, res) => { 
     const user = new User(req.body)
 
