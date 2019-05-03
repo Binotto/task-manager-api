5c29c9f201d8b0cd042aae286427bba642846796
Matheus <Binotto>
2019-05-03 18:57:39 -0300

    Logging in Users

--- src/routers/user.js
+++ src/routers/user.js
@@ -15,7 +15,14 @@ router.get('/users', async (req, res) => {
 })
 
 router.post('/users/login', async (req, res) => {
-
+    try {
+        //console.log(req.body)
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+        console.log(user)
+    } catch (error){
+        res.status(400).send(error)
+    }
 })
 
 //Finding a user by id
