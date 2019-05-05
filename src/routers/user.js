33f67a1db8f9c9e11775a491c9d9f2790094123a
Matheus <Binotto>
2019-05-05 19:36:43 -0300

    Logging Out

--- src/routers/user.js
+++ src/routers/user.js
@@ -19,6 +19,33 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+//Logout
+router.post('/users/logout', auth,  async (req, res) => {
+    try{
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        
+        await req.user.save()
+
+        res.send()
+    }catch (error){
+        res.status(500).send()
+    }
+})
+
+//Logout of all
+router.post('/users/logoutAll', auth, async (req, res) => {
+
+    try{
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    }catch (error){
+        res.status(500).send()
+    }
+})
+
 //Finding a user by id
 router.get('/users/:id', async (req, res) => {
     const _id = req.params.id
