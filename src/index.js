4e1202b6b4a4e69fb46a9b719cc1d744f4d73c98
Matheus <Binotto>
2019-05-05 13:01:58 -0300

    Express Middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET'){
+//         res.send('GET requests are disabled')
+//     }else{
+//         next()
+//     }
+// })
+
+// Usable when the site is under maintenance
+app.use((req, res, next) => {
+    res.status(503).send('The system is under maintenance. Please come back later.')
+
+})
+
 
 app.use(express.json())
 app.use(userRouter)
