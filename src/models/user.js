a4a4603e3900c1bfc9eeca6f231be5393558fa39
Matheus <Binotto>
2019-05-07 18:13:54 -0300

    Cascade Delete Tasks

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 
 const userSchema = new mongoose.Schema({
@@ -103,6 +104,17 @@ userSchema.pre('save', async function (next){
 
     next()
 })
+
+
+//Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+    const user = this
+
+    await Task.deleteMany({ owner: user._id })
+    
+    next()
+})
+
 
 const User = mongoose.model('User', userSchema)
 
