7a6bdfb0c5eb9ab5ae1850e872f2c54cf4c1399a
Matheus <Binotto>
2019-05-08 17:55:53 -0300

    Handling Express Errors

--- src/routers/user.js
+++ src/routers/user.js
@@ -108,6 +108,8 @@ const avatar = multer ({
 })
 router.post('/users/me/avatar', avatar.single('avatar'), (req, res) => {
         res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 module.exports = router
