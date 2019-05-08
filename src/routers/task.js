4d8f491f2b88183bb1c1f3136790b0ffc995a8bb
Matheus <Binotto>
2019-05-08 14:01:12 -0300

    Sorting Data

--- src/routers/task.js
+++ src/routers/task.js
@@ -25,11 +25,17 @@ router.get('/tasks/:id', auth, async (req, res) => {
 
 //GET /tasks?completed=true
 //GET /tasks?limit=10&skip=20
+//GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
-
+    const sort = {}
     if (req.query.completed){
         match.completed = req.query.completed === 'true'
+    }
+
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
     try{
         await req.user.populate({
@@ -37,7 +43,8 @@ router.get('/tasks', auth, async (req, res) => {
             match,
             options:{
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate()
         res.status(201).send(req.user.tasks)
