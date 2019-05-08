7d8c86cb782bfd2d972f4e7ffa0144e0a6358418
Matheus <Binotto>
2019-05-08 20:15:38 -0300

    Serving up File

--- src/routers/user.js
+++ src/routers/user.js
@@ -119,5 +119,20 @@ router.delete('/users/me/avatar', auth, avatar.single('avatar'), async(req, res)
     res.send('Your profile image has been deleted.')
 })
 
+router.get('/users/:id/avatar', async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id)
+
+        if(!user || !user.avatar){
+            throw new Error()
+        }
+
+        res.set('Content-type', 'image/jpg')
+        res.send(user.avatar)
+    }catch(error){
+        res.status(404).send()
+    }
+})
+
 module.exports = router
 
