dea83b7fe8e2936b183a08abcb461af8fe31c09f
Matheus <Binotto>
2019-05-08 17:00:14 -0300

    Validating File Uploads

--- playground/multer.js
+++ playground/multer.js
@@ -1,7 +1,45 @@
 const multer = require('multer')
 
+
+//Third example
+// const upload = multer ({
+//     dest: 'images',
+//     limits: {
+//         fileSize: 1000000
+//     },
+//     fileFilter(req, file, cb) {
+//         //match() allows regular expressions to be tested    
+//         if(!file.originalname.match(/\.(doc|docx)$/)) {
+//             return cb(new Error('File must a Word document.'))
+//         }
+        
+//         cb(undefined, true)
+//     }
+// })
+
+
+//Second example
+// const upload = multer ({
+//     dest: 'images',
+//     limits: {
+//         fileSize: 1000000
+//     },
+//     fileFilter(req, file, cb) {
+//         if(!file.originalname.endsWith('.pdf')) {
+//             return cb(new Error('File must a PDF.'))
+//         }
+        
+//         cb(undefined, true)
+//     }
+// })
+
+
+// First example
 const upload = multer ({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    }
 })
 app.post('/upload', upload.single('upload'),  (req, res) => {
     res.send()
--- src/index.js
+++ src/index.js
@@ -21,6 +21,27 @@ const port = process.env.PORT || 3000
 
 // })
 
+
+const multer = require('multer')
+
+const upload = multer ({
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('File must a Word document.'))
+        }
+        
+        cb(undefined, true)
+    }
+})
+
+app.post('/upload', upload.single('upload'),  (req, res) => {
+    res.send()
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
